f3306b5e7383ead3e680da893a6b031d1461129e
Tom X <419921393@qq.com>
2026-05-19 13:29:01 +0800

    新增情绪预测2.0.0

---
+++ src/main/java/com/weibo/utils/Api/CallBatchApi.scala
@@ -0,0 +1,9 @@
+package com.weibo.utils.Api
+
+/**
+ * @author Xbx
+ * @date 2026/5/19 11:42
+ */
+object CallBatchApi {
+
+}
---
+++ src/main/java/com/weibo/utils/hiveUtils/HiveTableCreate.scala
@@ -0,0 +1,9 @@
+package com.weibo.utils.hiveUtils
+
+/**
+ * @author Xbx
+ * @date 2026/5/19 11:40
+ */
+object HiveTableCreate {
+
+}
